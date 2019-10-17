aa3147c14a928e1668555a21d6708df926e7e5fb
Rodrigo Barreto Barbosa <rodrigo.barbosab@outlook.com>
2019-10-17 02:00:13 -0300

    Criadas classes iniciais do projeto

---
+++ LightEcommerce/Infra/Products.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LightEcommerce.Dados
+{
+    public class Products : TypeProduct
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public int Value { get; set; }
+        public TypeProduct Type { get; set; }
+
+        public Products() { }
+
+        [HttpGet("products")]
+        public IList<Products> GetProduct()
+        {
+            IList<Products> products = new List<Products>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var p = new Products
+                {
+                    Id = Convert.ToString(i),
+                    Name = Convert.ToString(i),
+                    Price = Convert.ToString(i),
+                    Value = i * i * i,
+                };
+
+                products.Add(p);
+            }
+
+            return products;
+        }
+    }
+}
